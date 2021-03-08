a62eacf92c82afe746d1b94def2431162346d90a
vsmaliakou <vsmaliakou@yandex.by>
2021-03-08 11:58:16 +0300

    54 -  life cycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,11 +13,10 @@ type UsersType = {
 
 class Users extends React.Component<UsersType> {
 
-    constructor(props: UsersType) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
     render() {
